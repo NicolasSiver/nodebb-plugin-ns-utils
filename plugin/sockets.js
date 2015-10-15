066a25be09c7af90de73e96055bd957a9a914436
Nicolas Siver <nicolas@siver.im>
2015-10-14 21:57:17 -0400

    server emit

--- plugin/sockets.js
+++ plugin/sockets.js
@@ -5,7 +5,9 @@
         controller = require('./controller'),
         nodebb     = require('./nodebb');
 
-    var adminSockets = nodebb.adminSockets;
+    var adminSockets  = nodebb.adminSockets,
+        serverSockets = nodebb.serverSockets,
+        emitNamespace = 'admin.plugins.' + constants.SOCKET_NAMESPACE + '.';
 
     Sockets.init = function (callback) {
         adminSockets[constants.SOCKET_NAMESPACE] = {};
@@ -13,12 +15,21 @@
         //Acknowledgements
         adminSockets[constants.SOCKET_NAMESPACE].chatsStatsGet = Sockets.chatsStatsGet;
         adminSockets[constants.SOCKET_NAMESPACE].primaryDbGet = Sockets.primaryDbGet;
+        adminSockets[constants.SOCKET_NAMESPACE].chatsPurgeStart = Sockets.chatsPurgeStart;
 
         callback();
     };
 
     Sockets.chatsStatsGet = function (socket, payload, callback) {
         controller.getChatsStats(callback);
+    };
+
+    Sockets.chatsPurgeStart = function (socket, payload, callback) {
+        controller.startChatsPurgeProcess(callback);
+    };
+
+    Sockets.emit = function (eventName, payload) {
+        serverSockets.emit(emitNamespace + eventName, payload);
     };
 
     Sockets.primaryDbGet = function (socket, payload, callback) {
