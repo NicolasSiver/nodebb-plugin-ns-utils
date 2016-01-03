3e408c3f5b58b6ed49f91cdf8e1789aba1a71b34
Nicolas Siver <nicolas@siver.im>
2016-01-03 10:11:15 -0500

    change socket method declaration

--- client/acp/models/socket-method.js
+++ client/acp/models/socket-method.js
@@ -1,5 +1,4 @@
-export default Object.freeze({
-    GET_CHATS_STATS  : 'admin.plugins.ns-utils.chatsStatsGet',
-    GET_DATABASE_NAME: 'admin.plugins.ns-utils.primaryDbGet',
-    START_CHATS_PURGE: 'admin.plugins.ns-utils.chatsPurgeStart'
-});
+export const GET_CHATS_STATS = 'admin.plugins.ns-utils.chatsStatsGet';
+export const GET_DATABASE_NAME = 'admin.plugins.ns-utils.primaryDbGet';
+export const START_CHATS_PURGE = 'admin.plugins.ns-utils.chatsPurgeStart';
+export const START_SANITIZE = 'admin.plugins.ns-utils.sanitizeStart';
